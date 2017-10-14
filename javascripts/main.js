ad6bac3072b5ea467150bf73651314e8544f630e
thejonroberts <jon.roberts.8@gmail.com>
2017-10-14 13:41:54 -0500

    import cohort info into grid

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,15 @@
 'use strict';
-// dependency libraries (bootstrap needs jQuery and Popper.js available globally)
-// window.jQuery = require('jquery');
-// window.Popper = require('Popper.js');
-// require('bootstrap');
-
+// dependencies
 let $ = require('jquery');
 const Handlebars = require('handlebars');
 
-let photogrid = require('../templates/photo_grid.hbs');
+// handlebars templates
 let loadNav = require('../templates/navbar.hbs');
+let photogrid = require('../templates/photo_grid.hbs');
+// student data in json file
+const { cohort } = require('../data/cohort.json');
 
 $('#navBar').append(loadNav);
-$('#photoGrid').append(photogrid);
+
+let studentInfoGrid = photogrid({ cohort });
+$('#photoGrid').append(studentInfoGrid);
